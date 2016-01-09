ec12be53dfc58ed6735f85fb7f1541b11287ed4d
Johann Weiser <johann.weiser@aon.at>
2016-01-09 10:40:53 +0100

    5 Hallos.

--- src/weiser/Test3.java
+++ src/weiser/Test3.java
@@ -4,7 +4,9 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo Förderkurs!");
-
+		System.out.println("Hallo Förderkurs!");
+		System.out.println("Hallo Förderkurs!");
+		System.out.println("Hallo Förderkurs!");
 	}
 
 }
